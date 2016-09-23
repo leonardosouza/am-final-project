5b3729f3bcffddfd3005e4edfbc2e16da1c7a98b
Vitor Costa <vitorcosta-pm@hotmail.com>
2016-09-22 21:04:37 -0300

    Adicionando JSON de clientes

--- sms-sender/main.js
+++ sms-sender/main.js
@@ -1,5 +1,5 @@
 var twilio = require("./node_modules/twilio/lib");
-
+var clients = require("./clients.json");
 
 var accountSid = 'AC1898c9f84b3e4ee1867143be6f8cfaa6'; // Your Account SID from www.twilio.com/console
 var authToken = '708716ab6f7b697365ab9106b77de15e';   // Your Auth Token from www.twilio.com/console
@@ -7,12 +7,26 @@ var authToken = '708716ab6f7b697365ab9106b77de15e';   // Your Auth Token from ww
 var twilio = require('twilio');
 var client = new twilio.RestClient(accountSid, authToken);
 
-
-client.messages.create({
-	body: "HEllo from node",
-	to: "+5511975846899",
-	from: "+12569527022"
-}, function(err, message){
-	console.log(err);
+clients.forEach(function(obj){
+    if(sendMessage(obj.phoneNumber, "Atenção! Seu carro está em movimento!")){
+        console.log("Message send success to customer ",obj.name);
+    }
 });
+
+function sendMessage(number, message){
+     client.messages.create({
+        body: message,
+        to: number,
+        from: "+12569527022"
+    }, function(err, message){
+        console.log(err);
+        return false;
+    });
+    
+    return true;
+};
+
+
+
+
 
