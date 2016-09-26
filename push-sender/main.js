b0772e31eb6e8f7f229efd8851817749fa73d4a1
Vitor Costa <vitorcosta-pm@hotmail.com>
2016-09-26 11:45:01 -0300

    Ajustando package.json e adicionando validação de idToken

--- push-sender/main.js
+++ push-sender/main.js
@@ -8,23 +8,38 @@ firebase.initializeApp({
 
 var serverKey = 'AIzaSyBJLI_z_WNuC6V_l-HNid7R7if-W2b2beI';
 var fcm = new FCM(serverKey);
+var idToken = "ebsEIYkBgPI:APA91bEdC4kIrLdmc1kHcbXZMk3qDJJS0lPkcTV4Pz0VL3x8NhSqmcCNF9m64XwBI5wgcR217EeDMa3nPna8K-GmKwUkVS4lKZu_0oSe9YPlPKZ7UBvVzU4PpV8mVkZz3ol_k32D3YnB";
+
+
 
 var message = {
-    to: "ebsEIYkBgPI:APA91bEdC4kIrLdmc1kHcbXZMk3qDJJS0lPkcTV4Pz0VL3x8NhSqmcCNF9m64XwBI5wgcR217EeDMa3nPna8K-GmKwUkVS4lKZu_0oSe9YPlPKZ7UBvVzU4PpV8mVkZz3ol_k32D3YnB",
+    to: idToken,
+    collapse_key: 'notification', 
+    data: {
+        movement: true
+    },
     notification: {
-        title: 'Carro em movimento',
-        body: 'Atenção!'
+        title: 'Atenção!',
+        body: 'Carro em movimento'
     }
 };
 
-//promise style
-var f = fcm.send;
-f(message).then(function(response){
-        console.log("Successfully sent with response: ", arguments);
-    })
-    .catch(function(err){
-        console.log("Something has gone wrong!",arguments);
-        console.error(err);
-    })
+var send = fcm.send;
 
+//valida idToken do dispostivo
+firebase.auth().verifyIdToken(idToken).then(function(decodedToken) {
+	var uid = decodedToken.uid;
+	console.log('UID',uid);
+	send(message).then(function(response){
+        	console.log("Successfully sent with response: ", arguments);
+   	 })
+    	.catch(function(err){
+       		console.log("Something has gone wrong!");
+        	console.error(err);
+    	})
+
+}).catch(function(error) {
+	console.error('idToken invalido');
+	console.error(error);
+});
 
