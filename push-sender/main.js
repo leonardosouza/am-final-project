d66ba84a6e5332542e56ca2abc5367ac2a9e3828
Vitor Costa <vitorcosta-pm@hotmail.com>
2016-09-26 22:06:48 -0300

    Enviando push-notification com sucesso
    Adicionada nova biblioteca para envio de notificações Push para Android;
    Removendo modules não utilizados;

--- push-sender/main.js
+++ push-sender/main.js
@@ -1,45 +1,22 @@
-var FCM = require ('./node_modules/fcm-push');
-var firebase = require ('./node_modules/firebase');
-
-firebase.initializeApp({
-  serviceAccount: "./NotifyCar-3545f3eeb2c7.json",
-  databaseURL: "https://notifycar-b9f1a.firebaseio.com/"});
-
+var FCM = require('fcm-node');
 
 var serverKey = 'AIzaSyBJLI_z_WNuC6V_l-HNid7R7if-W2b2beI';
 var fcm = new FCM(serverKey);
-var idToken = "ebsEIYkBgPI:APA91bEdC4kIrLdmc1kHcbXZMk3qDJJS0lPkcTV4Pz0VL3x8NhSqmcCNF9m64XwBI5wgcR217EeDMa3nPna8K-GmKwUkVS4lKZu_0oSe9YPlPKZ7UBvVzU4PpV8mVkZz3ol_k32D3YnB";
-
-
 
 var message = {
-    to: idToken,
-    collapse_key: 'notification', 
-    data: {
-        movement: true
-    },
+    to: 'cVcc-6tob8g:APA91bFob12Lr_obVpqWEWgGG8t9Egwi-QvkYU2DoLZOJVusgPvu81m0cCOaKXw9wNfHDtwS1EBeVFLq8t9wuRAw51zEUiDySY_kvXmwRkiuLTV4pKwLGzSAWbdxvGsl690mwsiwFT7A', 
+    collapse_key: 'carro',
     notification: {
-        title: 'Atenção!',
-        body: 'Carro em movimento'
-    }
+        title: 'Carro em movimento', 
+        body: 'Atenção!! Seu carro está em movimento' 
+    },
+
 };
 
-var send = fcm.send;
-
-//valida idToken do dispostivo
-firebase.auth().verifyIdToken(idToken).then(function(decodedToken) {
-	var uid = decodedToken.uid;
-	console.log('UID',uid);
-	send(message).then(function(response){
-        	console.log("Successfully sent with response: ", arguments);
-   	 })
-    	.catch(function(err){
-       		console.log("Something has gone wrong!");
-        	console.error(err);
-    	})
-
-}).catch(function(error) {
-	console.error('idToken invalido');
-	console.error(error);
+fcm.send(message, function(err, response){
+    if (err) {
+        console.log("Mensagem não enviada!");
+    } else {
+        console.log("Mensagem enviada com sucesso: ", response);
+    }
 });
-
